b4e787e3281dd12f15c0110bdaab0d8853e31417
Noah Hellman <noah.hellman@protonmail.com>
2018-11-07 10:36:01 +0100

    add Makefile and skeletons

---
+++ src/comm/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* opencv libs, might mv to separate opencv file */
+#include <cv.h>
+#include <highgui.h>
+
+int main(int argc, char* args[]) {
+    printf("Comm module hej hej\n");
+
+    return EXIT_SUCCESS;
+}
---
+++ src/ctrl/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <avr/io.h>
+
+int main(int argc, char* args[]) {
+    printf("sens module hej hej\n");
+
+    return EXIT_SUCCESS;
+}
