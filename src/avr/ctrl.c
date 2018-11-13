9f6391fe4382aba98112bc95f7357034415d7a51
Arvidsson <jakar180@student.liu.se>
2018-11-13 12:54:59 +0100

    fix pwm port & pd loop

--- src/avr/ctrl.c
+++ src/avr/ctrl.c
@@ -4,11 +4,18 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
+typedef struct {
+    float kp;           //Constant Pro
+    float kd;           //Constant Der
+    float err;          //Error
+    float last_err;     //Previous error
+} pd_values;
+
 void pwm_init(){
     
     //Initialize to phase and frequency correct PWM
-    TCCR1A |= (1<<WGM01)|(1<<WGM00)|(1<<COM1A1)|(1<<COM1B1);
-    TCCR1B |= (1<<WGM11)|(1<<CS00);
+    TCCR1A |= (1<<WGM10)|(1<<COM1A1)|(1<<COM1B1);
+    TCCR1B |= (1<<WGM13)|(1<<CS10);
     
     //Set PD4, PD5 to outputs
     DDRD |= (1<<PD4)|(1<<PD5);
@@ -18,7 +25,7 @@ void spi_init_slave(){
     //Set MISO as output
     DDRB |= (1<<PB6);
 
-    //Enable SPI    
+    //Enable SPI Interrupt & SPI    
     SPCR |= (1<<SPIE)|(1<<SPE);
 
     //Clear SPI Data
@@ -42,17 +49,40 @@ void init_jtagport(){
     DDRC |= (1<<PC4);
 }
 
+float pd_ctrl(pd_values *v){
+    float proportion;
+    float derivative;
+    proportion = v->err                 * v->kp;
+    derivative = v->err - v->last_err   * v->kd;
+    v->last_err = v->err;
+
+    return proportion + derivative;
+}
+
 int main(int argc, char* args[]) {
-    uint8_t duty_v = 0;
-    uint8_t duty_r = 0;
+    uint8_t duty_vel = 0;
+    uint8_t duty_rad = 0;
+
+    pd_values vel;
+    pd_values rad;
 
     pwm_init();
     spi_init_slave();
     init_lcdports();
 
+    //Enable interrupt
+    sei();
+    
     while(1){
-        OCR1A = duty_r;
-        OCR1B = duty_v;
+        
+        //vel->err = value sent from comm Velocity
+        //rad->err = value sent from comm Radius
+    
+        duty_vel = pd_ctrl(&vel);
+        duty_rad = pd_ctrl(&rad);
+
+        OCR1A = duty_rad;
+        OCR1B = duty_vel;
     }
     
     return 0;
