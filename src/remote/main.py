bb41400d12dc87de930391fa2f40af7f40bf6819
Dennis Derecichei <dende301@student.liu.se>
2018-11-16 16:48:58 +0100

    go back to previous code

--- src/remote/main.py
+++ src/remote/main.py
@@ -2,8 +2,8 @@ from course import Node, NodeType
 from remote import Command, Client
 
 def main():
-    client = Client('10.121.162.1', 5000)
-    client.send_command(Command.SET_MISSION, [1.8, 'kör', 'ööö'])
+    client = Client('10.121.164.152', 5000)
+    client.send_command(Command.SET_MISSION, [1.8, 'MADDAFAKKAAAA', 'ööö'])
 
 if __name__ == '__main__':
     main()
