088ed0bc1250eec9bdf0873761dfc00545e10497
Noah Hellman <noah.hellman@protonmail.com>
2018-11-19 13:32:21 +0100

    mv cmd protocol to separate file

--- src/comm/main.c
+++ src/comm/main.c
@@ -10,6 +10,8 @@
 #include "server.h"
 #include "ip/img_proc.h"
 
+#include "../spi/protocol.h"
+
 #define SERVER_PORT_START 9000
 #define SERVER_PORT_END 9100
 
@@ -29,25 +31,6 @@ struct sens_data_frame {
     sens_dist_t dist_right;
     sens_rot_t rotations;
 };
-
-/* flags for bus command;
- *  examples:
- *      write speed =   0000 0011 = 3
- *      write turn kp = 0000 0101 = 5 */
-#define BF_WRITE 1
-#define BF_SPEED 2
-#define BF_REG   4
-#define BF_KD    8
-
-/* bus cmd bytes */
-#define BCB_SPEED    BF_WRITE|BF_SPEED
-#define BCB_SPEED_KD BF_WRITE|BF_SPEED|BF_REG|BF_KD
-#define BCB_SPEED_KP BF_WRITE|BF_SPEED|BF_REG
-#define BCB_TURN     BF_WRITE
-#define BCB_TURN_KD  BF_WRITE|         BF_REG|BF_KD
-#define BCB_TURN_KP  BF_WRITE|         BF_REG
-#define BCB_GET_SENS 16
-#define BCB_RESET    32
 
 /* bus commands for ctrl */
 struct bus_cmd BC_SPEED =    {BCB_SPEED,    SLAVE_CTRL, sizeof(err_val_t)};
