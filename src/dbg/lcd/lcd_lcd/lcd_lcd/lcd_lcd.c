cc9d6267c289a43b54570679379065ac821f6d45
Emir Hadzisalihovic <bonte@lysator.liu.se>
2018-11-22 16:15:43 +0100

    LCD NOT WOKRING BUT IT SHOULD WORK, PETER HAS LOOKED INTO IT

--- src/dbg/lcd/lcd_lcd/lcd_lcd/lcd_lcd.c
+++ src/dbg/lcd/lcd_lcd/lcd_lcd/lcd_lcd.c
@@ -7,138 +7,170 @@
  * All characters in the rom are ASCII-encoded, 8 bits
  */ 
 
-
+#define F_CPU 16000000
 #include <avr/io.h>
 #include "lcd.h"
 
-//Transmit instruction set by already assigned values to databuss and RS
-void lcd_send_instruction(){
-	PORTD &= ~(1<<RS); // RS=0 when sending instructions
+void lcd_send_pulse(){
 	PORTD |= (1<<EN);
-	_delay_ms(1);
+	_delay_ms(10);
 	PORTD &= ~(1<<EN);
-	_delay_ms(1);
+	_delay_ms(10);
+}
+ 
+//Transmit instruction set by already assigned values to databuss and RS
+void lcd_send_instruction(unsigned char instr){
+	unsigned char upper = instr;
+	upper &= 0xF0;
+	upper &= ~(1<<RS);
+	
+	PORTD = upper;
+	lcd_send_pulse();
+	
+	unsigned char lower = instr;
+	lower = (lower << 4);
+	lower &= ~(1<<RS);
+	
+	PORTD = lower;
+	lcd_send_pulse();
 }
 
-void lcd_send_data(){
-	PORTD |= (1<<RS); //RS =1 when sending data to ram
-	PORTD |= (1<<EN);
-	_delay_ms(1);
-	PORTD &= ~(1<<EN);
-	_delay_ms(1);
+void lcd_set_4bit_interface(){
+	PORTD = 0x20;
+	PORTD &= ~(1<<RS);
+	lcd_send_pulse();
+}
+
+void lcd_send_data(unsigned char data){
+	unsigned char upper = data;
+	upper &= 0xF0;
+	upper |= (1<<RS);
+	
+	PORTD = upper;
+	lcd_send_pulse();
+	
+	unsigned char lower = data;
+	lower = (lower << 4);
+	lower |= (1<<RS);
+	
+	PORTD = lower;
+	lcd_send_pulse();
 }
 
 
 void lcd_busy_wait(){
 	//D5,D4 = 1, RS = 0 R/W = 0
-	PORTD &= 0x00; // Reset port values from previous
+	PORTD = 0x00; // Reset port values from previous
 	PORTD |= (1<<D5)|(1<<D4);
-	lcd_send_instruction();
+	PORTD &= ~(1<<RS);
+	lcd_send_pulse();
 }
 
-//initialize lcd in 4 bit mode, see page 13 in datasheet of hd44780 
-void lcd_init(){
-	// wait 15ms or more after power on
+void lcd_reset(){
+	//PORTD = 0xFF;
 	_delay_ms(20);
 	lcd_busy_wait();
-	_delay_ms(10); // wait 4.1ms or more
+	_delay_ms(5);
 	lcd_busy_wait();
-	_delay_ms(5);//wait 100 micro seconds or more
+	_delay_ms(1);
 	lcd_busy_wait();
-	
-	//STAGE 1 4-BIT FUNCTION SET
-	PORTD &= 0x00;
-	PORTD |= (1<<D5);
-	lcd_send_instruction();
-	_delay_ms(2);
-	
-	//STAGE 2 FUNCTION SET
-	PORTD &= 0x00;
-	PORTD |= (1<<D5);
-	lcd_send_instruction();
 	_delay_ms(1);
-	
-	PORTD &= 0x00;
-	PORTD |= (1<<D7)|(1<<D6);
-	lcd_send_instruction();
-	_delay_ms(1);
-	
-	//STAGE 3 DISPLAY OFF
-	PORTD &= 0x00;
-	//SEND just zeroes for some reason
-	lcd_send_instruction();
-	_delay_ms(1);
-	
-	PORTD |= (1<<D4);
-	lcd_send_instruction();
-	_delay_ms(2);
-	
-	//STAGE 4 DISPLAY CLEAR
-	PORTD &= 0x00;
-	lcd_send_instruction();
-	_delay_ms(2);
-	PORTD |= (1<<D4);
-	lcd_send_instruction();
-	_delay_ms(4);
-	
-	/*
-	STAGE 5 ENTRY MODE SET
-	SET I/D=1 CURSOR TO MOVE TO RIGHT AND DDRAM INCREASED BY 1
-	SET SH=0 SHIFTING OF ENTIRE DISPLAY IS OFF
-	*/
-	PORTD &=0x00;
-	lcd_send_instruction();
-	_delay_ms(2);
-	//D6=1 I/D=1 SH=0
-	PORTD |= (1<<D6)|(1<<D5);
-	lcd_send_instruction();
-	_delay_ms(2);
-	
-	//turn on lcd
-	PORTD = 0x00;
-	lcd_send_instruction();
-	PORTD = 0xF0;
-	lcd_send_instruction();
-	_delay_ms(3000);
 }
 
+
+void lcd_init(){
+	lcd_reset();
+
+	//STAGE 1
+	lcd_set_4bit_interface();
+	
+	//STAGE 2 4 bit mode 2 line 5x8 font
+	lcd_send_instruction(0x28);
+	
+	//STAGE 3 DISPLAY OFF
+	lcd_send_instruction(0x80);
+	
+	//STAGE 4 DISPLAY CLEAR
+	lcd_send_instruction(0x01);
+	
+	//STAGE 5 I/D = 1 D6 = 1 S = 0
+	lcd_send_instruction(0x06);
+}
+
+
+void lcd_clear(){
+	lcd_send_instruction(0x01);
+	_delay_ms(3);
+}
+
+/*
+void lcd_set_ddram(){
+	PORTD = 0x00;
+	lcd_send_instruction();
+	PORTD = 0x20;
+	lcd_send_instruction();
+}
+*/
+
+void lcd_display_on(){
+	//turn on lcd
+	lcd_send_instruction(0x0F);
+	_delay_ms(300);
+}
+
+void lcd_shift_right(){
+	lcd_send_instruction(0x14);
+}
 
 int main(void)
 {
-	_delay_ms(40);
-	
-	uint8_t counter;
-	
-	counter = 0;
-	
+		
 	/*All pin on port D are outputs to LCD except PD2 and PD3, inputs from halleffect 
-	  PORTA0 (pin 40) and PORTA1(pin 39) are inputs from sensors*/
+	  PORTD0 (pin 40) and PORTD1(pin 39) are inputs from sensors*/
 	DDRD = 0xFF;
 	DDRB = 0xFF;
+	DDRA = 0xFF;
 	//DDRA = 0x00; // inputs from sensors to AD converter
 	
     lcd_init();
+	_delay_ms(5);
 	
-	//send letter A
-	PORTD = 0x00;
-	PORTD |=(1<<RS);
-	lcd_send_data();
-	_delay_ms(1);
-	PORTD=0x10;
-	lcd_send_data();
-	_delay_ms(1);
+	// Display cursor, no blink
+	lcd_send_instruction(0x0F);
+	
+	
+	lcd_clear();
+	for(int i = 0; i < 4; i++){
+		_delay_ms(1000);
+		lcd_shift_right();
+	}
+	
+	
+	lcd_clear();
+	_delay_ms(100);
+	
+	lcd_send_data(0x41);
+	_delay_ms(20);
+	lcd_send_data(0x31);
+	
+	_delay_ms(20);
+	lcd_send_data(0x32);
 	
 	while(1)
     {
-		counter++;
-		PORTB = counter;
+		//lcd_send_data(0x41);
+		//_delay_ms(20);
 		
+		/*
+		PORTD = 0x42;
+		//PORTD |= (1<<RS);
+		lcd_send_pulse();
 		
-	
+		_delay_ms(10);
 		
-		
-		
-
-
-    }
+		PORTD = 0x12;
+		lcd_send_pulse();
+		_delay_ms(1);
+		*/
+	}
 }
