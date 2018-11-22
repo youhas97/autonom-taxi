4e1bc30ddd14c1dfc1d21ba2c3dcec08356daf83
Noah Hellman <noah.hellman@protonmail.com>
2018-11-23 00:20:46 +0100

    replace macro with for loop

--- src/tests/spi_protocol.c
+++ src/tests/spi_protocol.c
@@ -2,27 +2,37 @@
 
 #include "spi/protocol.h"
 
-#define BCS_MATCH(array, command) assert (array[command].cmd == command)
-
 int main(void) {
-    /* ctrl bcs are mapped correctly */
-    BCS_MATCH(BCCS, BB_INVALID);
-    BCS_MATCH(BCCS, BBC_RST);
-    BCS_MATCH(BCCS, BBC_SYN);
-    BCS_MATCH(BCCS, BBC_ROT_KD);
-    BCS_MATCH(BCCS, BBC_ROT_KP);
-    BCS_MATCH(BCCS, BBC_ROT_VAL);
-    BCS_MATCH(BCCS, BBC_ROT_ERR);
-    BCS_MATCH(BCCS, BBC_VEL_KD);
-    BCS_MATCH(BCCS, BBC_VEL_KP);
-    BCS_MATCH(BCCS, BBC_VEL_VAL);
-    BCS_MATCH(BCCS, BBC_VEL_ERR);
+    /* must contain all possible commands */
+    const int CTRL_CMDS[] = {
+        BB_INVALID,
+        BBC_RST,
+        BBC_SYN,
+        BBC_ROT_KD,
+        BBC_ROT_KP,
+        BBC_ROT_VAL,
+        BBC_ROT_ERR,
+        BBC_VEL_KD,
+        BBC_VEL_KP,
+        BBC_VEL_VAL,
+        BBC_VEL_ERR,
+    };
+    const int SENS_CMDS[] = {
+        BB_INVALID,
+        BBS_RST,
+        BBS_SYN,
+        BBS_GET,
+    };
 
-    /* sens bcs are mapped correctly */
-    BCS_MATCH(BCSS, BB_INVALID);
-    BCS_MATCH(BCSS, BBS_RST);
-    BCS_MATCH(BCSS, BBS_SYN);
-    BCS_MATCH(BCSS, BBS_GET);
+    /* check that commands are mapped correct */
+    for (int i = 0; i < sizeof(CTRL_CMDS)/sizeof(*CTRL_CMDS); i++) {
+        int command = CTRL_CMDS[i];
+        assert (BCCS[command].cmd == command);
+    }
+    for (int i = 0; i < sizeof(SENS_CMDS)/sizeof(*SENS_CMDS); i++) {
+        int command = SENS_CMDS[i];
+        assert (BCSS[command].cmd == command);
+    }
 
     return 0;
 }
