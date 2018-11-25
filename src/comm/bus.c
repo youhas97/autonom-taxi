686a86b5b20805cedf10e26429eec24c4d7361e8
Noah Hellman <noah.hellman@protonmail.com>
2018-11-25 20:20:56 +0100

    use linux api directly for SPI instead of wiringPi

--- src/comm/bus.c
+++ src/comm/bus.c
@@ -7,10 +7,11 @@
 
 #include <pthread.h>
 
-#ifdef PI
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
-#endif
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+#define SPI_DEVICE "/dev/spidev0."
 
 #define WAITTIME 1 /* seconds before wake up if nothing scheduled */
 
@@ -22,6 +23,8 @@ struct bus {
 
     pthread_cond_t wake_up;
     pthread_mutex_t wake_up_mutex;
+
+    int fds[2];
 
     struct order *queue;
     bool terminate;
@@ -51,24 +54,30 @@ struct order_blocked {
     pthread_mutex_t done_mutex;
 };
 
+static void spi_tranceive(int fd, void *src, void *dst, int len) {
+    struct spi_ioc_transfer transfer = {0};
+    transfer.tx_buf = (uint64_t)src;
+    transfer.rx_buf = (uint64_t)dst;
+    transfer.len = (uint32_t)len;
+
+    ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
+}
+
 /* physical bus functions (bus thread) */
-static bool receive(const struct bus_cmd *bc, void *msg) {
+static bool receive(int fd, const struct bus_cmd *bc, void *dst) {
     bool success = false;
 #ifdef PI
     cs_t cs = cs_create(bc->cmd, NULL, 0);
-    wiringPiSPIDataRW(bc->slave, (unsigned char*)&cs, 1);
-    wiringPiSPIDataRW(bc->slave, (unsigned char*)msg, bc->len);
-    success = cs_check(cs, msg, bc->len);
+    spi_tranceive(fd, (void*)&cs, NULL, 1);
+    spi_tranceive(fd, NULL, dst, bc->len);
+    success = cs_check(cs, dst, bc->len);
 #else
-    for (int i = 0; i < bc->len; i++) {
-        *((uint8_t*)msg+i) = (uint8_t)i;
-    }
     success = true;
 #endif
     return success;
 }
 
-static bool transmit(const struct bus_cmd *bc, void *msg) {
+static bool transmit(int fd, const struct bus_cmd *bc, void *msg) {
     bool success = false;
     /*
     printf("transmit:\n");
@@ -76,20 +85,15 @@ static bool transmit(const struct bus_cmd *bc, void *msg) {
         printf("%02x ", ((uint8_t*)msg)[i]);
     printf("\n");
     */
-#ifdef PI
     cs_t cs = cs_create(bc->cmd, msg, bc->len);
+#ifdef PI
+    spi_tranceive(fd, (void*)&cs, NULL, 1);
+
+    if (bc->len > 0)
+        spi_tranceive(fd, (void*)msg, NULL, bc->len);
+
     uint8_t ack;
-
-    /* send cmd sum */
-    wiringPiSPIDataRW(bc->slave, (unsigned char*)&cs, 1);
-
-    /* send data, if any */
-    if (bc->len > 0) {
-        wiringPiSPIDataRW(bc->slave, (unsigned char*)msg, bc->len);
-    }
-
-    /* retrieve ack */
-    wiringPiSPIDataRW(bc->slave, (unsigned char*)&ack, 1);
+    spi_tranceive(fd, NULL, (void*)&ack, 1);
 
     success = (ack == ACKS[bc->slave]);
 #else
@@ -135,29 +139,6 @@ static void order_queue(struct bus *bus, struct order *o, bool requeue) {
     pthread_mutex_unlock(&bus->lock);
 }
 
-/* execute an order, from bus thread */
-static bool order_execute(struct bus *bus, struct order *o) {
-    packets_sent++;
-    bool success = false;
-    if (o->bc->write) {
-        void *data_copy = malloc(o->bc->len);
-        memcpy(data_copy, o->src_dst, o->bc->len);
-        success = transmit(o->bc, data_copy);
-        if (success)
-            memcpy(o->src_dst, data_copy, o->bc->len);
-        free(data_copy);
-    } else {
-        success = receive(o->bc, o->src_dst);
-    }
-    if (!success)
-        packets_lost++;
-
-    printf("packet loss: %.1f\n",
-           ((float)packets_lost/(float)packets_sent)*100);
-
-    return success;
-}
-
 /* bus thread function */
 
 static void *bus_thread(void *b) {
@@ -179,7 +160,21 @@ static void *bus_thread(void *b) {
 
         /* execute order if any, otherwise go to sleep */
         if (order) {
-            bool success = order_execute(bus, order);
+            bool success = false;
+            int fd = bus->fds[order->bc->slave];
+            if (order->bc->write) {
+                success = transmit(fd, order->bc, order->src_dst);
+            } else {
+                success = receive(fd, order->bc, order->src_dst);
+            }
+
+            if (!success)
+                packets_lost++;
+            packets_sent++;
+
+            printf("packet loss: %.1f\n",
+               ((float)packets_lost/(float)packets_sent)*100);
+
             if (success) {
                 if (order->scheduled) {
                     struct order_scheduled *os = (struct order_scheduled*)order;
@@ -219,11 +214,17 @@ struct bus *bus_create(int freq) {
     pthread_cond_init(&bus->wake_up, NULL);
     pthread_mutex_init(&bus->wake_up_mutex, NULL);
 
-    /* setup spi */
+    /* setup spi for each slave */
 #ifdef PI
-    wiringPiSetup();
-    wiringPiSPISetup(0, freq);
-    wiringPiSPISetup(1, freq);
+    bus->fds[0] = open(SPI_DEVICE "0", O_RDWR);
+    bus->fds[1] = open(SPI_DEVICE "1", O_RDWR);
+    for (int i = 0; i < 2; i++) {
+        uint8_t mode = SPI_MODE_0;
+        uint8_t bpw = 8;
+        ioctl(bus->fds[i], SPI_IOC_WR_MODE, &mode);
+        ioctl(bus->fds[i], SPI_IOC_WR_BITS_PER_WORD, &bpw);
+        ioctl(bus->fds[i], SPI_IOC_WR_MAX_SPEED_HZ, &freq);
+    }
 #endif
 
     /* start bus thread */
