7332b75454fce78f102e06049a409bdd1095662e
Noah Hellman <noah.hellman@protonmail.com>
2018-11-25 19:10:20 +0100

    fix errors, debug spi

--- src/dbg/packet_loss/comm.c
+++ src/dbg/packet_loss/comm.c
@@ -10,55 +10,32 @@
 #include <wiringPiSPI.h>
 #endif
 
-#define CHANNEL 0 /* channel to use on RPI */
-#define SS 8
-
-#define F_SPI 700000
-
-#define CMD 0xc
-
-static bool transmit(void *msg) {
-    bool success = false;
-#ifdef PI
-    cs_t cs = cs_create(CMD, msg, bc->len);
-    digitalWrite(SS, 1);
-    digitalWrite(SS, 0);
-    wiringPiSPIDataRW(CHANNEL, (unsigned char*)&cs, 1);
-    wiringPiSPIDataRW(CHANNEL, (unsigned char*)msg, DATA_LENGTH);
-    uint8_t ack;
-    wiringPiSPIDataRW(CHANNEL, (unsigned char*)&ack, 1);
-    digitalWrite(SS, 1);
-    success = (ack == ACK);
-#else
-    success = ((rand() % 100) < 50);
-#endif
-    return success;
-}
+#define F_SPI 1000000
 
 int main(void) {
-#ifdef PI
-    wiringPiSetupGpio();
-    wiringPiSPISetup(CHANNEL, F_SPI);
-#endif
+    wiringPiSetup();
+    wiringPiSPISetup(0, F_SPI);
 
     int packets_lost = 0;
     int packets_sent = 0;
 
-    srand(time(NULL));
-    uint8_t msg[DATA_LENGTH];
-
-    for (int i = 0; i < 1000; i++) {
-        for (int j = 0; j < DATA_LENGTH; j++) {
-            msg[j] = rand() % 256;
-        }
-        if (!transmit((void*)msg)) {
-            packets_lost++;
-        }
-        packets_sent++;
+    for (int i = 0; i < 10; i++) {
+        uint8_t cmd = 0x01;
+        uint8_t data[] = {0x02, 0x03};
+        uint8_t ack = 0x04;
+        printf("-----------------------\n");
+        printf("%02x | %02x %02x | %02x\n", cmd, data[0], data[1], ack);
+        wiringPiSPIDataRW(0, (unsigned char*)&cmd, 1);
+        wiringPiSPIDataRW(0, (unsigned char*)data, 2);
+        wiringPiSPIDataRW(0, (unsigned char*)&ack, 1);
+        printf("%02x | %02x %02x | %02x\n", cmd, data[0], data[1], ack);
     }
 
+    packets_lost++;
+    packets_sent++;
+
     float loss = (float)packets_lost / (float)packets_sent;
-    printf("packet loss: %.1f%%\n", loss*100);
+    printf("packet loss: %.2f%%\n", loss*100);
 
     return 0;
 }
