bd2cb94216ddd2f64fac430f7b622441f520e174
Arvidsson <jakar180@student.liu.se>
2018-11-30 11:54:45 +0100

    add time dependence in obj functions

--- src/comm/objective.c
+++ src/comm/objective.c
@@ -1,16 +1,24 @@
 #include "objective.h"
 
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #include "main.h"
 
 #define brake_dist 40 //distance from line when braking starts          (cm)
 #define still_dist 0  //distance from line when car should be still     (cm)
-#define LEFT -100
-#define RIGHT 100
+#define STRAIGHT 0
+#define LEFT -1
+#define RIGHT 1
 #define stop_vel 0
 #define slow_vel 40
 #define full_vel 100
+
+#define CRT CLOCK_REALTIME
+#define SEC 1000000000
+
 
 bool obj_ignore(struct car_state *state, struct data_ctrl *ctrl, void *data);
 bool obj_stop(struct car_state *state, struct data_ctrl *ctrl, void *data);
@@ -28,6 +36,8 @@ const struct obj OBJS[] = {
     {"exit", obj_exit},
 };
 const int OBJC = sizeof(OBJS)/sizeof(*OBJS);
+
+struct timespec start, finish;
 
 struct obj_item *objq_create(int cmdc, char **cmds) {
     bool valid = true;
@@ -68,9 +78,8 @@ void objq_destroy(struct obj_item *queue) {
 }
 
 ctrl_val_t wtd_speed(float distance, float current, float target) {
-    //TODO Calculate smooth transition from current to desired speed
-    float new_vel = 0;
-    return new_vel;
+    //TODO update if there is no distance(no stopline)
+    return (target-current)/distance;
 }
 
 bool obj_ignore(struct car_state *state, struct data_ctrl *ctrl, void *data) {
@@ -96,11 +105,22 @@ bool obj_park(struct car_state *state, struct data_ctrl *ctrl, void *data) {
     sens_dist_t stop_dist = state->ip->stopline_dist; 
     float cur_vel = state->sens->velocity;
     if (stop_dist <= still_dist) {
-        ctrl->rot.value = 1;
-        //Wait until car is 45 degrees somehow (time?)
-        //Wait until straight
-        ctrl->vel.value = wtd_speed(stop_dist, cur_vel, stop_vel);
-        return true;
+        if(data == NULL)
+            clock_gettime(CRT, &start); 
+        else
+            start.tv_nsec = (long int)data;
+        ctrl->rot.value = RIGHT;
+        clock_gettime(CRT, &finish);
+    
+        if(cur_vel == 0)
+            return true;
+        else if((finish.tv_nsec - start.tv_nsec) >= SEC){
+            ctrl->vel.value = wtd_speed(stop_dist, cur_vel, stop_vel);
+            ctrl->rot.value =STRAIGHT;
+        }
+        else if((finish.tv_nsec - start.tv_nsec) >= SEC*0.5)
+            ctrl->rot.value = LEFT;
+        data = &start.tv_nsec;
     } else if (stop_dist <= brake_dist) {
         ctrl->vel.value = wtd_speed(stop_dist, cur_vel, slow_vel);
     }
@@ -109,21 +129,43 @@ bool obj_park(struct car_state *state, struct data_ctrl *ctrl, void *data) {
 
 bool obj_unpark(struct car_state *state, struct data_ctrl *ctrl, void *data) {
     sens_dist_t stop_dist = state->ip->stopline_dist; 
-    float cur_vel = state->sens->velocity;
-    ctrl->rot.value = -1;
-    //Wait until car is 45 degrees somehow (time?)
-    //Wait until straight
-    ctrl->vel.value = wtd_speed(stop_dist, cur_vel, full_vel);
-    return true;
+    float cur_vel = state->sens->velocity; 
+    if(data == NULL)
+        clock_gettime(CRT, &start); 
+    else
+        start.tv_nsec = (long int)data;
+    ctrl->rot.value = LEFT;
+    ctrl->vel.value = wtd_speed(stop_dist, cur_vel, slow_vel);
+    clock_gettime(CRT, &finish);
+    
+    if(cur_vel == full_vel)
+        return true;
+    else if((finish.tv_nsec - start.tv_nsec) >= SEC){
+        ctrl->vel.value = wtd_speed(stop_dist, cur_vel, full_vel);
+        ctrl->rot.value = STRAIGHT;
+    }
+    else if((finish.tv_nsec - start.tv_nsec) >= SEC*0.5)
+        ctrl->rot.value = RIGHT;
+    data = &start.tv_nsec;
+    return false;
 }
 
 bool obj_enter(struct car_state *state, struct data_ctrl *ctrl, void *data) {
     sens_dist_t stop_dist = state->ip->stopline_dist; 
     float cur_vel = state->sens->velocity;
     if (stop_dist <= still_dist) {
-        ctrl->rot.value = 1;
-        //Wait for some time
-        return true;
+        if(data == NULL)
+            clock_gettime(CRT, &start); 
+        else
+            start.tv_nsec = (long int)data;
+        ctrl->rot.value = RIGHT;
+        clock_gettime(CRT, &finish);
+    
+        if((finish.tv_nsec - start.tv_nsec) >= SEC*0.5){
+            ctrl->rot.value = STRAIGHT;
+            return true;
+        }
+        data = &start.tv_nsec;
     } else if (stop_dist <= brake_dist) {
         ctrl->vel.value = wtd_speed(stop_dist, cur_vel, slow_vel);
     }
@@ -134,11 +176,20 @@ bool obj_exit(struct car_state *state, struct data_ctrl *ctrl, void *data) {
     sens_dist_t stop_dist = state->ip->stopline_dist;
     float cur_vel = state->sens->velocity;
     if (stop_dist <= still_dist) {
-        ctrl->rot.value = 1;
-        //Wait for some time
-        return true;
-    } else if (stop_dist <= brake_dist) {
-        ctrl->vel.value = wtd_speed(stop_dist, cur_vel, slow_vel);
-    }
+        if(data == NULL)
+            clock_gettime(CRT, &start); 
+        else
+            start.tv_nsec = (long int)data;
+        ctrl->rot.value = RIGHT;
+        clock_gettime(CRT, &finish);
+    
+        if(cur_vel == full_vel)
+            return true;
+        else if((finish.tv_nsec - start.tv_nsec) >= SEC*0.5){
+            ctrl->vel.value = wtd_speed(stop_dist, cur_vel, full_vel);
+            ctrl->rot.value = STRAIGHT;
+        }
+        data = &start.tv_nsec;
+    }     
     return false;
 }
