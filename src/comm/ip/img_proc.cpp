f836b0e8536e91b264bc6976cb446a792bd7ddda
Noah Hellman <noah.hellman@protonmail.com>
2018-12-05 19:17:05 +0100

    Revert "line pos x: replace median with weighted average (by length)"
    This reverts commit 39b2587ccd6461e911c721b7092ac9594aa5d641.

--- src/comm/ip/img_proc.cpp
+++ src/comm/ip/img_proc.cpp
@@ -383,32 +383,26 @@ T median(std::vector<T> v) {
     return median;
 }
 
-/* weighted average x position of lines at y=height
- * weighted by length of line */
+/* median x position of lines at y=height */
 int line_pos_x(lines_t lines, int height) {
     if (lines.empty()) {
         return -1;
     } else {
-        double total_pos = 0;
-        double total_weight = 0;
+        std::vector<int> pos;
         for (auto l : lines) {
             cv::Point s(l[0], l[1]), e(l[2], l[3]);
-            double x = 0;
-            double weight = (s.x-e.x)*(s.x-e.x)+(s.y-e.y)*(s.y-e.y);
             if (s.x == e.x) {
-                x = s.x;
-            } else if (s.y == e.y) {
-                weight = 0;
-            } else {
+                pos.push_back(s.x);
+            } else if (s.y != e.y) {
                 double k = (double)(e.y-s.y) / (e.x-s.x);
                 double m = s.y-k*s.x;
-                x = (height-m)/k;
+                pos.push_back((int)((height-m)/k));
             }
-            total_pos += weight*x;
         }
-        return total_pos == 0 ? -1 : (int)(total_pos/total_weight);
+        return pos.empty() ? -1 : median(pos);
     }
 }
+
 
 /* median y position of lines at x=width */
 int line_pos_y(lines_t lines, int width) {
