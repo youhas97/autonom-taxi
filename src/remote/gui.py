16222427e8c76e458a5fe04c1247b2eb5883df85
Yousef Hashem <youhas97@gmail.com>
2018-12-12 18:57:29 +0100

    fix removal of edges/nodes in gui

--- src/remote/gui.py
+++ src/remote/gui.py
@@ -104,6 +104,14 @@ class Map():
                 self.path += new_path[1:]
             else:
                 self.path += new_path
+
+            path_type = []
+            for node in self.path:
+                path_type += [node.type]
+
+            print("mission: ", new_mission)
+            
+            print("path: ", path_type)
             
         elif self.selected_node and current_node:
             if not self.edge_exists(self.selected_node, current_node) and self.selected_node != current_node:
@@ -118,6 +126,7 @@ class Map():
         if(self.selected_node in self.nodes):
             self.nodes.remove(self.selected_node)
         if self.selected_edge in self.edges:
+            self.selected_edge.start.outgoing.remove(self.selected_edge)
             self.edges.remove(self.selected_edge)
         for edge in self.edges:
                 if edge.start not in self.nodes and edge.end not in self.nodes:
@@ -179,7 +188,7 @@ class Map():
         elif(node_start and node_end):
             edge = GraphEdge(node_start, node_end, cost)
             self.edges.append(edge)
-            node_start.addEdge(node_end, cost)
+            node_start.addEdge(edge)
             self.selected_node = None
             self.draw()
             
@@ -396,12 +405,16 @@ class GUI():
     def set_mission(self):
         print("SELECT START AND DESTINATION")
         self.map.select_mission = True
+        self.map.selected_node = None
+        self.mao.selected_node = None
     
     def clear_mission(self): 
         self.map.path = []
         self.map.mission = []
         self.map.sub_path = []
         self.tasks.put(Task.CLEAR_MISSION)
+        self.map.selected_node = None
+        self.map.selected_edge = None
         self.map.draw()
 
     def button_down(self, direction):
